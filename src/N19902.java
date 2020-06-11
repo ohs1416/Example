b9c5b4056e674694b9d568a335b2e56f07c0b61c
Oheller <ohs1416@naver.com>
2020-06-11 14:20:25 +0900

    백준

--- src/N19902.java
+++ src/N19902.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
-public class Main {
+public class N19902 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,7 +26,6 @@ public class Main {
             sb.append('\n');
 
         }
-
         System.out.println(sb);
     }
 }
